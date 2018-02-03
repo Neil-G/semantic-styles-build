ecf6bf6696e05194ee8668956de31cdf20e18f69
Warren Neil Gordon <neil@Warrens-MacBook-Pro.local>
2018-02-02 20:33:58 -0500

    rename compiled css to index.css and index.min.css

--- tasks/config/tasks.js
+++ tasks/config/tasks.js
@@ -19,9 +19,9 @@ module.exports = {
   },
 
   filenames: {
-    concatenatedCSS            : 'semantic.css',
+    concatenatedCSS            : 'index.css',
     concatenatedJS             : 'semantic.js',
-    concatenatedMinifiedCSS    : 'semantic.min.css',
+    concatenatedMinifiedCSS    : 'index.min.css',
     concatenatedMinifiedJS     : 'semantic.min.js',
     concatenatedRTLCSS         : 'semantic.rtl.css',
     concatenatedMinifiedRTLCSS : 'semantic.rtl.min.css'
